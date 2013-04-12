caab9b16b73b62ed7b5fbc2c67926d388aaed7a7
Rohan Shah <rmshah91@gmail.com>
2013-04-11 21:15:17 -0400

    add all the files

---
+++ gong.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var arduino = require('duino');
+var board = new arduino.Board({debug:true});
+var led = new arduino.Led({
+	board: board,
+	pin: 13
+});
+
+app.post('/gong/', function(req, res){
+	res.status(200).end('Rang The Gong');
+	led.blink();
+});
+
+app.listen(3000);
+console.log('Listenting at port 3000');
+
